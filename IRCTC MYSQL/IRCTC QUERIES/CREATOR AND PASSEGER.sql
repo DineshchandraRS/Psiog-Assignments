d2b72991e6ed2920a9e1d57b230bea9d71548e0a
Dinesh Chandra <139257944+DineshchandraRS@users.noreply.github.com>
2024-11-02 01:48:03 +0530

    Delete IRCTC MYSQL/IRCTC QUERIES directory

--- IRCTC MYSQL/IRCTC QUERIES/CREATOR AND PASSEGER.sql
+++
@@ -1,6 +0,0 @@
-USE MYDB;
-SELECT PASSENGER_ID,name,passenger.GENDER,seat,status,userid,creator.FIRSTNAME,creator.LASTNAME,creator.GMAIL,creator.MOBILE,creator.STATE,creator.ADDRESS
-from passenger
-inner join creator on passenger.PASSENGER_ID=creator.PASSENGERSIDS
-order by PASSENGER_ID;
-
--- IRCTC MYSQL/IRCTC QUERIES/TRAIN AND PAYMENT.sql
+++
@@ -1,5 +0,0 @@
-use mydb;
-select train.ADMINSID,train.DESTINATION,train.DEPARTURE_DAY,train.TRAIN_NAME,train.TRAIN_TYPE,payment.BOOKING_ID,payment.PASS_ID,payment.PAYMENT_MODE,payment.TRAINNUMBERSSSSS
-from train
-inner join payment on payment.TRAINNUMBERSSSSS=train.TRAIN_NUMBER
-order by TRAIN_NUMBER;
