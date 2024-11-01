9c4be63908dd31662d430ef1c35dbecbc6b8a5b4
Dinesh Chandra <139257944+DineshchandraRS@users.noreply.github.com>
2024-11-02 01:51:37 +0530

    Delete NUTTY CRUD-02/NUTTYCRUD-P02-01 directory

--- NUTTY CRUD-02/NUTTYCRUD-P02-01/NUTTYCRUD-P02-01/Entry.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NUTTYCRUD_P02_01
-{
-    internal class Entry
-    {
-        public string Username { get; internal set; }
-        public string Password { get; internal set; }
-        public object Role { get; internal set; }
-    }
-}
